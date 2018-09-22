20894bb55a15de98a2c2872a9ca92a2718e9cca4
Rainerino <albertyanyy@gmail.com>
2018-09-22 14:19:14 -0700

    update CI

---
+++ tools/setup-ci.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# Make sure script is run as root
+if [ "${EUID}" -ne 0 ]; then
+    echo "Error: Please run with root."
+    exit 1
+fi
+
+# Check programs are installed
+if [ ! -x "$(command -v git)" ]; then
+    echo "Git is not installed. Installing..."
+    apt install git -y
+    echo "Git installed"
+fi
+if [ ! -x "$(command -v python3)" ]; then
+    echo "Python 3.6 is not installed. Installing..."
+    apt install python3.6 python3.6-dev -y
+    echo "Python 3.6 installed"
+fi
+if [ ! -x "$(command -v pip3)" ]; then
+    echo "Python3-pip is not installed. Installing..."
+    apt install python3-pip -y
+    echo "Python3-pip installed"
+fi
+if [ ! -x "$(command -v pipenv)" ]; then
+    echo "Pipenv is not installed. Installing..."
+    pip3 install pipenv
+    echo "Pipenv installed"
+fi
+
+
+# Get root directory of project
+CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && cd .. && pwd)"
+
+sudo -u "${USER}" git submodule update --init --recursive
+echo "Submodules updated"
+
+# Start postgres and check user/database
+sudo
+
+"${CWD}"/tools/setup_db.sh
+service postgresql start
+
+echo "Checking migrations..."
+yes | sudo -u "${USER}" pipenv run python ../manage.py makemigrations
+sudo -u "${USER}" pipenv run python ../manage.py migrate
+
+
+# Load database with some data
+echo "Loading database..."
+sudo -u "${USER}" pipenv run python manage.py loaddata "${CWD}"/mavlink/fixtures/mavlinks.json
+
+
--- tools/setup.sh
+++ tools/setup.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+set -e
 
 # Make sure script is run as root
 if [ "${EUID}" -ne 0 ]; then
@@ -9,17 +11,17 @@ fi
 # Check programs are installed
 if [ ! -x "$(command -v git)" ]; then
     echo "Git is not installed. Installing..."
-    apt install git -y
+    apt install git -qq -y
     echo "Git installed"
 fi
-if [ ! -x "$(command -v python3)" ]; then
+if [ ! -x "$(command -v python3.6)" ]; then
     echo "Python 3.6 is not installed. Installing..."
-    apt install python3.6 python3.6-dev -y
+    apt install python3.6 -qq -y
     echo "Python 3.6 installed"
 fi
 if [ ! -x "$(command -v pip3)" ]; then
     echo "Python3-pip is not installed. Installing..."
-    apt install python3-pip -y
+    apt install python3-pip -qq -y
     echo "Python3-pip installed"
 fi
 if [ ! -x "$(command -v pipenv)" ]; then
@@ -27,25 +29,69 @@ if [ ! -x "$(command -v pipenv)" ]; then
     pip3 install pipenv
     echo "Pipenv installed"
 fi
+if [ ! -x "$(command -v npm)" ]; then
+    echo "npm not installed. Installing..."
+    apt install curl software-properties-common -qq -y
+    curl -sL https://deb.nodesource.com/setup_8.x | bash -
+    apt install nodejs -qq -y
+    echo "Nodejs installed"
+fi
+apt install python-dev libpq-dev postgresql postgresql-contrib -qq -y
 
 # Get root directory of project
 CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && cd .. && pwd)"
 
-sudo -u "${USER}" git submodule update --init --recursive
+# Start setup
+echo "Checking submodules..."
+cd "${CWD}"
+
+git submodule update --init --recursive
 echo "Submodules updated"
 
+# Create settings and env file if not existent
+SETTINGS_FILE="${CWD}"/Smurf/settings.py
+if [ ! -f "${SETTINGS_FILE}" ]; then
+    echo "Creating settings file..."
+    cp "${SETTINGS_FILE}".example "${SETTINGS_FILE}"
+    echo "Created "${SETTINGS_FILE}""
+fi
+ENV_FILE="${CWD}"/.env
+if [ ! -f "${ENV_FILE}" ]; then
+    echo "Creating env file..."
+    cp "${ENV_FILE}".example "${ENV_FILE}"
+    echo "Created "${ENV_FILE}""
+fi
+
+# Install dependences
+echo "Checking Python dependencies..."
+pipenv install --dev --3.6
+pipenv install
+echo "Checking JavaScript dependencies..."
+npm install
+
 # Start postgres and check user/database
-
-"${CWD}"/tools/setup_db.sh
 service postgresql start
+"${CWD}"/tools/setup_db.sh
 
+# Make migrations if necessary
 echo "Checking migrations..."
-yes | sudo -u "${USER}" pipenv run python ../manage.py makemigrations
-sudo -u "${USER}" pipenv run python ../manage.py migrate
-
+yes | pipenv run python manage.py makemigrations --merge
+pipenv run python manage.py migrate
 
 # Load database with some data
 echo "Loading database..."
-sudo -u "${USER}" pipenv run python manage.py loaddata "${CWD}"/mavlink/fixtures/mavlinks.json
+pipenv run python manage.py loaddata "${CWD}"/mavlink/fixtures/mavlinks.json
 
+# Create symlink for gcom-run use
+#GCOM_RUN_BIN=/usr/local/bin/gcom-run
+#
+#echo "Creating symlink for gcom-run..."
+#if [ -h "${GCOM_RUN_BIN}" ]; then
+#    echo "Symlink already exists, replacing..."
+#    rm "${GCOM_RUN_BIN}"
+#fi
+#
+#ln -s "${PWD}"/tools/gcom-run.sh "${GCOM_RUN_BIN}"
+#echo "Created symlink in "${GCOM_RUN_BIN}" for "${PWD}"/tools/gcom-run.sh"
 
+# Generate staticfiles/mediafiles directories
