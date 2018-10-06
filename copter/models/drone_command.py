195663e4844cbb64f84b0cdca51c734c97a2d838
Rainerino <albertyanyy@gmail.com>
2018-10-05 22:30:28 -0700

    tested one view

--- copter/models/drone_command.py
+++ copter/models/drone_command.py
@@ -1,3 +1,4 @@
+import sys
 import traceback
 
 import dronekit
@@ -35,9 +36,12 @@ class DroneCommand(models.Model):
 	@preconditions(lambda self: self.is_attempt_connect)
 	def connect_to_vehicle(self):
 		"""This function will connect to a physical drone
+			if vehicle is already connected, the function will return false with is_attempt_connect off, and not
+				modifying anything
 			precondition:
 			is_attempt_connect is set to True
-			is_attempt_disconnect is set to False
+			the physical drone that is connected to the port should be ABSOLUTELY READY TO GO
+				checklist include: gps lock, armable,
 
 			postcondition:
 			is_attempt_connect is set to False
@@ -46,14 +50,28 @@ class DroneCommand(models.Model):
 			is_attempt_disarm is False
 			The vehicle object is has passed all the checks
 
+
 			Args:
 			Return:
 				Boolean Connection result. If failed it will print an error message
 		"""
+		if DroneStatus.objects.get(pk=1).is_connected:
+			if Settings.ENGINE_DEBUG:
+				print("The copter is already connected")
+			self.is_attempt_connect = False
+			return False
+		if Settings.ENGINE_DEBUG:
+			print("starting to connect vehicle")
+
 		connect_object = DroneStatus.objects.get(pk=1)
 		connect_object.connection_status_message = Messages.INITIALIZATION
 		connect_object.is_connecting = True
+		connect_object.is_connected = False
+		connect_object.is_armed = False
+		connect_object.is_arming = False
 		connect_object.save()
+
+		no_error = True
 
 		try:
 			connection_port = self.connection_port
@@ -64,103 +82,94 @@ class DroneCommand(models.Model):
 				print("Connecting to %s " % connection_port)
 			# for some reason the connection fails some times
 
-			connect_object.vehicle = dronekit.connect(connection_port, wait_ready=False, heartbeat_timeout=10)
-			connect_object.save()
-			connect_object.vehicle.wait_ready(True, timeout=Settings.TIMEOUT)
-
-		# # API Error
-		# except dronekit.APIException:
-		# 	call_command('loaddata', 'copter_test')
-		#
-		# 	if Settings.ENGINE_DEBUG:
-		# 		print('Timeout when connecting!')
-		# 	connect_object.connection_status_message = Messages.CONNECTION_TIMEOUT
-		#
-		# 	connect_object.attempt_connect = False
-		#
-		# 	connect_object.save()
-		#
-		# 	return False
-
-		except Exception as e:
-
-			# if connect_object.vehicle is not None:
-			# 	connect_object.vehicle.close()
-
-			traceback.print_tb(e.__traceback__)
-
-			# failed to connect, reset everything and display mesage
-
-			connect_object.is_connecting = False
-
-			connect_object.is_connected = False
-
-			connect_object.connection_status_message = Messages.FAILED_CONNECTION % self.connection_port
-
+			# there are some really hard requirements for this
+			connect_object.vehicle = dronekit.connect(connection_port, wait_ready=True, heartbeat_timeout=10)
 			connect_object.save()
 
-			return False
-		else:
-			# Connection is success
-			connect_object.connection_status_message = Messages.CONNECTED % connection_port
+		except:
+			no_error = False
+		finally:
+			if no_error:
+				# Connection is success
+				connect_object.connection_status_message = Messages.CONNECTED % self.connection_port
 
-			connect_object.is_connected = True
+				connect_object.is_connected = True
 
-			connect_object.is_connecting = False
+				connect_object.is_connecting = False
 
-			connect_object.is_armed = False
+				connect_object.is_armed = False
 
-			connect_object.is_arming = False
+				connect_object.is_arming = False
 
-			connect_object.save()
+				connect_object.save()
 
-			self.is_attempt_connect = False
-			self.is_attempt_arm = False
-			self.is_attempt_disarm = False
-			self.is_attempt_disconnect = False
+				self.is_attempt_connect = False
+				self.is_attempt_arm = False
+				self.is_attempt_disarm = False
+				self.is_attempt_disconnect = False
 
-			self.save()
+				self.save()
 
-			if Settings.ENGINE_DEBUG:
-				print("Connected to %s " % connection_port)
+				if Settings.ENGINE_DEBUG:
+					print("Connected to %s " % self.connection_port)
 
-			return True
+				return True
+			else:
+				connect_object.is_connecting = False
+
+				connect_object.is_connected = False
+
+				connect_object.vehicle = None
+
+				connect_object.connection_status_message = Messages.FAILED_CONNECTION % self.connection_port
+
+				connect_object.save()
+
+				self.is_attempt_connect = False
+				self.is_attempt_arm = False
+				self.is_attempt_disarm = False
+				self.is_attempt_disconnect = False
+
+				self.save()
+
+				if Settings.ENGINE_DEBUG:
+					print("=====================================================================")
+					print("ERROR WAS HANDLED")
+
+				return False
+
+	@preconditions(lambda self, delay: self.is_attempt_arm and not self.is_attempt_disarm and isinstance(delay, int))
+	def arm_vehicle(self, delay=0):
+		""""
+		Arm vehicle function
+
+		precondition: vehicle is connected, and user is attempt arm
+		postcondition: vehicle is armed
+
+		Args:
+			delay: the duration of the delay to wait before arming
+		Return:
+			bool indicate if arming failed or not
+		"""
+		pass
+
+	def disarm_vehicle(self):
+		""""Disarm vehicle function
+		precondition: vehicle is connected, and user is attempt arm
+		postcondition: vehicle is armed
+
+		Args:
+			delay: the duration of the delay to wait before arming
+		Return:
+			bool indicate if arming failed or not
+		"""
+		pass
+
+	def disconnect_from_vehicle(self):
+		"""This function will disconnect from a connected vehicle
+		precondition: connected to a vehicle
+		postcondition:
 
 
-@preconditions(lambda self, delay: self.is_attempt_arm and not self.is_attempt_disarm and isinstance(delay, int))
-def arm_vehicle(self, delay=0):
-	""""
-	Arm vehicle function
-
-	precondition: vehicle is connected, and user is attempt arm
-	postcondition: vehicle is armed
-
-	Args:
-		delay: the duration of the delay to wait before arming
-	Return:
-		bool indicate if arming failed or not
-	"""
-	pass
-
-
-def disarm_vehicle(self):
-	""""Disarm vehicle function
-	precondition: vehicle is connected, and user is attempt arm
-	postcondition: vehicle is armed
-
-	Args:
-		delay: the duration of the delay to wait before arming
-	Return:
-		bool indicate if arming failed or not
-	"""
-	pass
-
-
-def disconnect_from_vehicle(self):
-	"""This function will disconnect from a connected vehicle
-	precondition: connected to a vehicle
-	postcondition:
-
-
-	"""
-	pass
+		"""
+		pass
--- copter/views/view_command_mode.py
+++ copter/views/view_command_mode.py
@@ -3,21 +3,35 @@ from rest_framework.response import Response
 
 from copter.serializers.drone import DroneCommandSerializer
 from copter.models.drone_command import DroneCommand
+from copter.models.drone_status import DroneStatus
 from rest_framework import generics
 
+# https://www.google.ca/imgres?imgurl=https%3A%2F%2Fwww.steveschoger.com%2Fstatus-code-poster%2Fimg%2Fstatus-code.png&imgrefurl=https%3A%2F%2Fwww.steveschoger.com%2Fstatus-code-poster%2F&docid=3kGsGg01rqjQxM&tbnid=0JKfplsTmQaMIM%3A&vet=10ahUKEwiih9Dm-vDdAhUprVQKHSQTDvkQMwhFKAEwAQ..i&w=1224&h=1710&client=ubuntu&bih=904&biw=1920&q=http%20code&ved=0ahUKEwiih9Dm-vDdAhUprVQKHSQTDvkQMwhFKAEwAQ&iact=mrc&uact=8
 
-class DroneCommandListCreate(generics.RetrieveUpdateDestroyAPIView):
+class DroneCommandListCreate(generics.RetrieveUpdateAPIView):
 	queryset = DroneCommand.objects.all()
 	serializer_class = DroneCommandSerializer
 
-	def retrieve(self, request, *args, **kwargs):
+	def put(self, request, *args, **kwargs):
 		serializer = DroneCommandSerializer(data=request.data)
 		if serializer.is_valid():
 			serializer.save()
 
-		copter = serializer.validated_data
-		# TODO more conditions
-		if copter.get('is_attempt_connect'):
-			DroneCommand.objects.get(pk=1).connect_to_vehicle()
+		# First save everything
 
-		return Response(serializer.data, status=200)
+		if serializer.validated_data.get('is_attempt_connect'):
+			print("======================")
+			connection_port = serializer.validated_data.get("connection_port")
+			copter = DroneCommand.objects.get(pk=1)
+			copter.connection_port = connection_port
+			copter.is_attempt_connect = True
+			copter.save()
+
+			copter.connect_to_vehicle()
+
+		if DroneStatus.objects.get(pk=1).is_connected:
+			print("Connected!")
+			return Response(serializer.data, status=202)
+		else:
+			print("Failed")
+			return Response(serializer.data, status=503)
